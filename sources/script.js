9e1e42e02f5e63eeda1e9515ea5706086fa37cdf
Raynecloudss <rayne.a.fall@gmail.com>
2024-01-12 19:57:05 -0500

    moment added, java added

--- sources/script.js
+++ sources/script.js
@@ -1,1 +1,10 @@
-//oh it only works here??
+//Chicago
+let chicagoElement = document.querySelector("#chicago");
+let chicagoElementTime = chicagoElement.querySelector(".time");
+let chicagoElementDate = chicagoElement.querySelector(".date");
+let chicagoTime = moment();
+
+chicagoElementDate.innerHTML = moment().format("MMM Do YYYY");
+chicagoElementTime.innerHTML = `${chicagoTime.format(
+  "h:mm:ss"
+)}<small>${chicagoTime.format("A")}</small>`;
