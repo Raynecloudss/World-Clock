0903687ab271f2c7f1536a99e3964128b18fe80c
Raynecloudss <rayne.a.fall@gmail.com>
2024-01-12 20:11:11 -0500

    Current date/time added

--- sources/script.js
+++ sources/script.js
@@ -1,10 +1,26 @@
-//Chicago
-let chicagoElement = document.querySelector("#chicago");
-let chicagoElementTime = chicagoElement.querySelector(".time");
-let chicagoElementDate = chicagoElement.querySelector(".date");
-let chicagoTime = moment();
+function updateTime() {
+  //Chicago
+  let chicagoElement = document.querySelector("#chicago");
+  let chicagoElementTime = chicagoElement.querySelector(".time");
+  let chicagoElementDate = chicagoElement.querySelector(".date");
+  let chicagoTime = moment().tz("America/Chicago");
 
-chicagoElementDate.innerHTML = moment().format("MMM Do YYYY");
-chicagoElementTime.innerHTML = `${chicagoTime.format(
-  "h:mm:ss"
-)}<small>${chicagoTime.format("A")}</small>`;
+  chicagoElementDate.innerHTML = moment().format("MMM Do YYYY");
+  chicagoElementTime.innerHTML = `${chicagoTime.format(
+    "h:mm:ss"
+  )}<small>${chicagoTime.format("A")}</small>`;
+
+  //Paris
+  let parisElement = document.querySelector("#paris");
+  let parisElementTime = parisElement.querySelector(".time");
+  let parisElementDate = parisElement.querySelector(".date");
+  let parisTime = moment().tz("Europe/Paris");
+
+  parisElementDate.innerHTML = moment().format("MMM Do YYYY");
+  parisElementTime.innerHTML = `${parisTime.format(
+    "h:mm:ss"
+  )}<small>${parisTime.format("A")}</small>`;
+}
+
+updateTime();
+setInterval(updateTime, 1000);
